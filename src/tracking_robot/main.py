c2bc2c2c2dd625b9433d636a92e2eead35983d2b
yuzhen chen <yuzhench@umich.edu>
2024-10-17 21:44:04 -0400

    move to pi5

--- src/tracking_robot/main.py
+++ src/tracking_robot/main.py
@@ -34,7 +34,9 @@ def onclick(event):
 def setup_arguments():
     parser = argparse.ArgumentParser(description="choose the mode ")
 
-    parser.add_argument('-c', '--choose', type=bool, default=True)    
+    parser.add_argument('-c', '--choose', type=bool, default=True)   
+
+    parser.add_argument('-m', '--mode', type=str,default = "real_time") #real_time or process_video
     return parser
 
 
@@ -141,6 +143,97 @@ def process_video(input_video_path, output_video_path, model, tracker, colors, a
 
 
 
+def process_camera(model, tracker, colors, args):
+    global first_frame, clicked_x, clicked_y, target_id
+
+    def mouse_click(event, x, y, flags, param):
+        global clicked_x, clicked_y, target_id
+        if event == cv2.EVENT_LBUTTONDOWN:
+            clicked_x, clicked_y = x, y
+            target_id = None
+
+    # Open the camera (0 for internal, 1 for external camera)
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Unable to open camera")
+        return
+
+    # Get the video properties (frame width, height, and fps)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    print ("fps = ", fps)
+
+    # Define the codec and create VideoWriter object for output video
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output_camera_video.mp4', fourcc, fps, (width, height))
+
+    # Create a window and set a mouse callback to handle inputs
+    cv2.namedWindow('Camera Frame with YOLO')
+    cv2.setMouseCallback('Camera Frame with YOLO', mouse_click)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # First frame: choose the target
+        # if first_frame:
+        #     # if args.choose == True:
+        #     choose_the_person(frame)
+        #     first_frame = False
+
+        # else:
+        if clicked_x is not None and clicked_y is not None:
+            results = model(frame)
+            boxes = results[0].boxes
+            detections = []
+            
+            for i, (curr_xyxy, conf, cls_id) in enumerate(zip(boxes.xyxy, boxes.conf, boxes.cls)):
+                x1, y1, x2, y2 = int(curr_xyxy[0]), int(curr_xyxy[1]), int(curr_xyxy[2]), int(curr_xyxy[3])
+                
+                # Only track "person" class
+                if model.names[int(cls_id)] == 'person':
+                    detections.append([x1, y1, x2, y2, conf.item()])
+
+            tracker.update(frame, detections)
+
+            for track in tracker.tracks:
+                bbox = track.bbox
+                x1, y1, x2, y2 = bbox
+                track_id = track.track_id
+                
+                if target_id is None and if_in_bbox(x1, y1, x2, y2):
+                    target_id = track_id
+
+                if args.choose:
+                    if track_id == target_id:
+                        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), colors[track_id % len(colors)], 2)
+                        cv2.putText(frame, f'person {track_id}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
+                    else:
+                        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+                else:
+                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), colors[track_id % len(colors)], 2)
+                    cv2.putText(frame, f'person {track_id}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
+
+        # Show the frame with the bounding boxes
+        cv2.imshow('Camera Frame with YOLO', frame)
+
+        # Write the frame into the output file
+        out.write(frame)
+
+        # Break the loop on 'q' key press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the camera and output video
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
 def main():
 
     parser = setup_arguments()
@@ -152,8 +245,13 @@ def main():
 
     #generate the color
     colors = load_color('color.json')
-    # Process the video
-    process_video('../../video/p.mp4', 'output_video_with_tracking_choose.mp4', model, tracker, colors, args)
+
+    if args.mode == "process_video":
+        # Process the video
+        process_video('../../video/me.mp4', 'output_video_with_tracking_choose.mp4', model, tracker, colors, args)
+    elif args.mode == "real_time":
+        process_camera(model, tracker, colors, args)
+        
 
 
 
